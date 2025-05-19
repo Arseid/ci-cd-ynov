2089559c5b233f59c79e51c6656535fa9ee341a6
Arseid <yvesrobert.estrada@gmail.com>
2025-05-19 14:03:43 +0200

    Cours 7

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import React, {useEffect, useState} from 'react';
 import RegistrationForm from './components/registrationForm/RegistrationForm';
 import UserList from './components/userList/UserList';
 import './App.css';
+import axios from "axios";
 
 function App() {
     const [users, setUsers] = useState([]);
+    const port = process.env.REACT_APP_SERVER_PORT || 8000;
+    const [usersCount, setUsersCount] = useState(0);
+
+    useEffect(() => {
+        const countUsers = async () => {
+            try {
+                const api = axios.create({
+                   baseURL: `http://localhost:${port}`,
+                });
+                const response = await api.get('/users');
+                setUsersCount(response.data.utilisateurs.length);
+            } catch (error) {
+                console.error('Error fetching users count:', error);
+            }
+        }
+        countUsers();
+    }, [port]);
 
     const addUser = (user) => {
         const updatedUsers = [...users, user];
@@ -19,11 +37,17 @@ function App() {
 
     return (
         <div className="App">
-            <div className="component">
-                <RegistrationForm addUser={addUser} />
-            </div>
-            <div className="component">
-                <UserList users={users} />
+            <header className="App-Header">
+                <h1>Users Manager</h1>
+                <p>{usersCount} user(s) already registered</p>
+            </header>
+            <div className="App-Body">
+                <div className="component">
+                    <RegistrationForm addUser={addUser} />
+                </div>
+                <div className="component">
+                    <UserList users={users} />
+                </div>
             </div>
         </div>
     );
