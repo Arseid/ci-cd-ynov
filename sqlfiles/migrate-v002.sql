7c3395919f01e6a49a999a9b74063072343038d2
Arseid <yvesrobert.estrada@gmail.com>
2025-06-13 14:31:18 +0200

    fix migrate file with auto increment

--- sqlfiles/migrate-v002.sql
+++ sqlfiles/migrate-v002.sql
@@ -1,7 +1,7 @@
 USE ynov_ci;
 CREATE TABLE user
 (
-    id INT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     surname VARCHAR(100),
     name VARCHAR(100),
     email VARCHAR(255),
--- sqlfiles/migrate-v003.sql
+++ sqlfiles/migrate-v003.sql
@@ -1,3 +1,3 @@
 USE ynov_ci;
-INSERT INTO user (id, surname, name, email, birthdate, city, postal_code)
-VALUES (1, "fenoll", "loise", "loise.fenoll@ynov.com", "2000-01-01", "Nice", "06000");
+INSERT INTO user (surname, name, email, birthdate, city, postal_code)
+VALUES ("fenoll", "loise", "loise.fenoll@ynov.com", "2000-01-01", "Nice", "06000");
