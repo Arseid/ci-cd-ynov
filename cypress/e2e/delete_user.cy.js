77d819fab1ff8d0b87e564128aa28020ebc52f8c
Arseid <yvesrobert.estrada@gmail.com>
2025-06-22 17:34:53 +0200

    fix delete_user.cy.js

--- cypress/e2e/delete_user.cy.js
+++ cypress/e2e/delete_user.cy.js
@@ -2,6 +2,15 @@ describe("Gestion des suppressions d'utilisateurs", () => {
     const adminEmail = Cypress.env('TEST_ADMIN_EMAIL');
     const adminPassword = Cypress.env('TEST_ADMIN_PASSWORD');
 
+    const testUser = {
+        name: "Temp",
+        surname: "Userdelete",
+        email: "temp.user@example.com",
+        birthdate: "2000-01-01",
+        city: "Testville",
+        postal_code: "12345"
+    };
+
     beforeEach(() => {
         cy.visit('http://localhost:3000');
     });
@@ -24,12 +33,25 @@ describe("Gestion des suppressions d'utilisateurs", () => {
 
         cy.contains(`Logged in as ${adminEmail}`).should('exist');
 
-        cy.get('ul li').should('have.length.greaterThan', 0);
-
-        cy.get('button').contains('Supprimer').first().click();
+        cy.get('[data-testid="registration-form"]').within(() => {
+            cy.get('input[name="name"]').type(testUser.name);
+            cy.get('input[name="surname"]').type(testUser.surname);
+            cy.get('input[name="email"]').type(testUser.email);
+            cy.get('input[name="birthdate"]').type(testUser.birthdate);
+            cy.get('input[name="city"]').type(testUser.city);
+            cy.get('input[name="postal_code"]').type(testUser.postal_code);
+            cy.get('button[type="submit"]').contains('Sauvegarder').click();
+        });
 
         cy.wait(500);
 
-        cy.get('ul li').should('not.exist');
+        cy.contains(`${testUser.name} ${testUser.surname} - ${testUser.email}`).should('exist');
+
+        cy.contains('li', `${testUser.name} ${testUser.surname} - ${testUser.email}`)
+            .within(() => {
+                cy.contains('Supprimer').click();
+            });
+
+        cy.contains(`${testUser.name} ${testUser.surname} - ${testUser.email}`).should('not.exist');
     });
 });
